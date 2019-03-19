6a2ea1303a9c3c517fdb0783d245d07f4c8454e3
Sergey Kuzmin <s.kuzmin@fxclub.com>
2019-03-19 12:09:57 +0700

    Add current track notification after track changing

--- src/playerCtrls.js
+++ src/playerCtrls.js
@@ -1,21 +1,35 @@
 (() => {
     'use strict';
 
-    ipcRenderer.on('player', (e, ctrl) => {
-        switch (ctrl) {
-            case 'MediaPlayPause':
-                externalAPI.togglePause();
-                break;
-            case 'MediaPreviousTrack':
-                if (externalAPI.getProgress().position >= 5) {
-                    externalAPI.setPosition(0);
-                } else {
-                    externalAPI.prev();
-                }
-                break;
-            case 'MediaNextTrack':
-                externalAPI.next();
-                break;
-        }
+    const currentWebContents = require('electron').remote.getCurrentWindow().webContents;
+    const notifier = require('node-notifier');
+
+    currentWebContents.on('did-finish-load', () => {
+        ipcRenderer.on('player', (e, ctrl) => {
+            switch (ctrl) {
+                case 'MediaPlayPause':
+                    externalAPI.togglePause();
+                    break;
+                case 'MediaPreviousTrack':
+                    if (externalAPI.getProgress().position >= 5) {
+                        externalAPI.setPosition(0);
+                    } else {
+                        externalAPI.prev();
+                    }
+                    break;
+                case 'MediaNextTrack':
+                    externalAPI.next();
+                    break;
+            }
+
+            let track = externalAPI.getCurrentTrack();
+            notifier.notify({
+                title: track.title,
+                message: track.artists[0].title,
+                icon: 'https://' + track.cover.replace("%%", "100x100"),
+                sound: false,
+                wait: false
+            });
+        });
     });
 })();
